f5f23e19f0d3e2d216e6805efbed8cb955413fac
Mamus Eferha <eferhamamus@gmail.com>
2020-03-23 15:31:32 +0100

    javascript task

---
+++ main.js
@@ -0,0 +1,9 @@
+let name= "Eferha Mamus";
+let courses = ["HTML", "CSS", "JS", "PHP", "NODE.JS"];
+
+console.log(name);
+console.log(courses);
+
+for (let i = 1; i <= 200; i += 2) {
+  console.log(i);
+}
